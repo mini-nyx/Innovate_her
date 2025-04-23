ec91b9ccd5561a57c12de5d96f291384002de7bd
mini-nyx <aishwaryamukherjee33@gmail.com>
2025-04-23 20:19:53 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -9,7 +9,6 @@ from dotenv import load_dotenv
 from langchain_community.tools import TavilySearchResults
 
 
-os.chdir(r"D:\Aishwarya\Work\MachineHack\Chubb\files")  # Change to the directory where your .env file is located
 load_dotenv()  # Load variables from .env file
 
 print(os.getcwd())  # Print the current working directory to confirm it's correct
